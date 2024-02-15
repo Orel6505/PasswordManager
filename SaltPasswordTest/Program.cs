998839d9961b567aedcb1a6a364802864710a6d2
Orel Yosupov <Orely4242@gmail.com>
2024-02-15 21:16:52 +0200

    SecurityHelper: Test Password object

--- SaltPasswordTest/Program.cs
+++ SaltPasswordTest/Program.cs
@@ -11,16 +11,13 @@ namespace SaltPasswordTest
     {
         static void Main()
         {
-            Salt salt = new Salt();
-            string stl = salt.GenerateSalt(128);
-            string Password = "12345678";
-            string hashed = salt.GenerateHash(Password, stl);
-            Console.WriteLine(hashed);
-
             //sha256 returns hash of 256 bits or 64 bytes
             //256bits == 64bytes
-            Console.WriteLine(hashed.Length);
-            if (hashed == salt.GenerateHash(Password, stl))
+            SecurityHelper helper = SecurityHelper.GetInstance;
+            Password password = new Password("Orel6505");
+            
+            string TestPassword = Console.ReadLine();
+            if (password.HashPassword == helper.GenerateHash(TestPassword,password.Salt))
             {
                 Console.WriteLine("It worked");
             }
