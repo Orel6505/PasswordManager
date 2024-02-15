ca61109e70eb1b07f7c37c4c6a6d4b4f7717f929
Orel Yosupov <Orely4242@gmail.com>
2024-02-15 18:31:15 +0200

    PasswordSalt: Create Salt.cs

---
+++ SaltPassword/Salt.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaltPassword
+{
+    public class Salt : ISalt
+    {
+        public string GenerateHash(string Password, string Salt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GenerateSalt(int Length)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
