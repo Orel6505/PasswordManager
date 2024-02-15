dc5c2a34953eef4c31cef142eb6ec784e1437609
Orel Yosupov <Orely4242@gmail.com>
2024-02-15 19:47:52 +0200

    SaltPassword: Add Summerize

--- SaltPassword/Salt.cs
+++ SaltPassword/Salt.cs
@@ -9,6 +9,8 @@ namespace SaltPassword
 {
     public class Salt : ISalt
     {
+        /// <summary>Creates Random Salt</summary>
+        /// <returns>Returns random array of letters with length of the value given</returns>
         public string GenerateSalt(int Length)
         {
             string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -19,6 +21,8 @@ namespace SaltPassword
             return salt;
         }
 
+        /// <summary>Takes <paramref name="Password"/> and <paramref name="Salt"/> and Hashes it with SHA256 Hash Alghoritm</summary>
+        /// <returns>Returns a hashed string</returns>
         public string GenerateHash(string Password, string Salt)
         {
             using (HashAlgorithm sha256 = SHA256.Create())
