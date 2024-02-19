cad37b91834e8a5a46cb24dd339f6fd7a5f10613
Orel Yosupov <Orely4242@gmail.com>
2024-02-19 20:29:35 +0200

    PasswordManager: Create Unit Of work

---
+++ WebServer/DAL/UnitOfWork.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebServer.DAL;
+
+namespace WebServer.DAL
+{
+    public class UnitOfWork
+    {
+        UserRepository userRepository;
+        UserTypeRepository userTypeRepository;
+
+        DbContext dbContext;
+        public UnitOfWork(DbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public UserRepository UserRepository
+        {
+            get
+            {
+                if (userRepository == null)
+                {
+                    this.userRepository = new UserRepository(dbContext);
+                }
+                return userRepository;
+            }
+        }
+
+        public UserTypeRepository UserTypeRepository
+        {
+            get
+            {
+                if (userTypeRepository == null)
+                {
+                    this.userTypeRepository = new UserTypeRepository(dbContext);
+                }
+                return userTypeRepository;
+            }
+        }
+    }
+}
