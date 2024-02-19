4a74f83ec262bb2be1f3bc3959825fb01bf3423b
Orel Yosupov <Orely4242@gmail.com>
2024-02-19 18:38:19 +0200

    PasswordManager: Test ModelFactory

--- PasswordManagerTest/Program.cs
+++ PasswordManagerTest/Program.cs
@@ -1,10 +1,14 @@
 ﻿using PasswordManager;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebServer.DAL.DBContext;
+using WebServer.DAL.ModelFactory;
+using WebServerModels;
 
 namespace PasswordManagerTest
 {
@@ -12,11 +16,15 @@ namespace PasswordManagerTest
     {
         static void Main()
         {
+            CheckModelFactory();
+        }
+        static void CheckPasswordManager()
+        {
             //sha256 returns hash of 256 bits or 64 bytes
             //256bits == 64bytes
             SecurityHelper helper = SecurityHelper.GetInstance;
-            Password password = new Password(helper,"Orel6505",64);
-            
+            Password password = new Password(helper, "Orel6505", 64);
+
             string EnteredPassword = Console.ReadLine();
             if (password.IsSamePassword(EnteredPassword))
             {
@@ -26,6 +34,24 @@ namespace PasswordManagerTest
             {
                 Console.WriteLine("Invalid Credentials");
             }
+        }
+
+        static void CheckModelFactory()
+        {
+            SqliteDbContext sqliteDbContext = SqliteDbContext.GetInstance();
+            ModelFactory modelFactory = new ModelFactory();
+            string sql = "Select * from Users";
+            sqliteDbContext.OpenConnection();
+            IDataReader dataReader = sqliteDbContext.Read(sql);
+            List<User> users = new List<User>();
+            while (dataReader.Read() == true)
+            {
+                User user = modelFactory.UserModelCreator.CreateModel(dataReader);
+                users.Add(user);
+            }
+            sqliteDbContext.CloseConnection();
+            foreach (User o in users)
+                Console.WriteLine($"{o.PasswordHash}");
         }
     }
 }
