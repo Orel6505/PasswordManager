b90c1739882af603880fff3960bfbe8e6373ab81
Orel Yosupov <Orely4242@gmail.com>
2024-02-19 16:54:48 +0200

    PasswordManager: Initial models

---
+++ WebServerModels/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServerModels
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string PasswordSalt { get; set; }
+        public string PasswordHash { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int UserTypeId { get; set; }
+        public string CreationDate { get; set; }
+    }
+}
